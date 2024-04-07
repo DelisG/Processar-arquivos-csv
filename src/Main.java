a85ebb4d843c1bdf130e33ffb658c088baa04629
DelisG <delisemarciano@gmail.com>
2024-04-07 14:26:57 -0300

    💫 Validação e movimentação de arquivos CSV

--- src/Main.java
+++ src/Main.java
@@ -16,5 +16,18 @@ public class Main {
             System.err.println("O diretório de PENDENTES não existe ou não é um diretório válido.");
             return;
         }
+        File[] arquivosCSV = dir.listFiles((dir1, nome) -> nome.toLowerCase().endsWith(".csv"));
 
+        if (arquivosCSV != null) {
+            for (File arquivo : arquivosCSV) {
+                if (validarCSV(arquivo)) {
+                    moverArquivo(arquivo, diretorioValidado);
+                    System.out.println("Arquivo " + arquivo.getName() + " validado e movido para /VALIDADO");
+                } else {
+                    moverArquivo(arquivo, diretorioInvalidado);
+                    System.out.println("Arquivo " + arquivo.getName() + " inválido e movido para /INVALIDADO");
+                }
+            }
+        }
+    }
 }
